1cd8aa3db8888861801b7d63d31009edcffc20fc
norlandogamez <norlandogamez1@gmail.com>
2024-03-23 16:05:57 -0700

    Buttons created

--- AssetManager/src/application/Main.java
+++ AssetManager/src/application/Main.java
@@ -3,39 +3,42 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-	    //Home page
+	//Home page
+	    StackPane root = new StackPane();
 	    
+        //view assets button
+	    Button view_assets = new Button("View Assets");
 	    
-	        //view assets button
-	    
-	        //add new asset button
-	    
-	        //view categories button
-	    
-	        //add new category button
-	            //This button will lead to a new stage/page
-	    
-	        //Add icon image
+        //add new asset button
+	    Button new_asset = new Button("Add New Asset");
+        //view categories button
+        Button view_categories = new Button("View Categories");
+        //add new category button
+            //This button will lead to a new stage/page
+        Button add_new_category = new Button("Add New Category");
+        //Add icon image
 	        
-	    //Add new category page
-	        //Category name textfield
-	            //error if left empty after pressing save
-	    
-	    
-	        //Category Description textfield
-	            //error if left empty after pressing save
-	    
-	        //save button
-	            //When pressed, will close this page and lead back to homepage
-	    
-	        //cancel button
-	            
+	//Add new category page
+        //Category name textfield
+            //error if left empty after pressing save
+        
+    
+        //Category Description textfield
+            //error if left empty after pressing save
+        
+        //save button
+            //When pressed, will close this page and lead back to homepage
+        Button save = new Button("Save");
+        //cancel button
+        Button cancel = new Button("Cancel");
 	    
 	    
 	    
