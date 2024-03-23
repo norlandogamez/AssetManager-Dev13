1d5fc81449995e891a5d29525a63a1b9909e3b57
norlandogamez <norlandogamez1@gmail.com>
2024-03-23 16:31:11 -0700

    Updated with separate pages.

--- AssetManager/src/application/Main.java
+++ AssetManager/src/application/Main.java
@@ -4,15 +4,18 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 	//Home page
-	    StackPane root = new StackPane();
+	    FlowPane root = new FlowPane();
 	    
         //view assets button
 	    Button view_assets = new Button("View Assets");
@@ -25,8 +28,31 @@ public class Main extends Application {
             //This button will lead to a new stage/page
         Button add_new_category = new Button("Add New Category");
         //Add icon image
-	        
+	    
+        
+        root.getChildren().addAll(view_assets, new_asset, view_categories, add_new_category);
+        
+        Scene scene = new Scene(root, 500, 500);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Home Page");
+        primaryStage.show();
 	//Add new category page
+        
+        
+        add_new_category.setOnAction(e->{
+            
+            StackPane root2 = new StackPane();
+        
+            Label label = new Label("Add New category page");
+            root2.getChildren().add(label);
+            Scene secondScene = new Scene(root2, 500, 500);
+            Stage secondStage = new Stage();
+            
+            secondStage.setScene(secondScene);
+            secondStage.setTitle("Add New Category Page");
+            secondStage.show();
+        });
+        
         //Category name textfield
             //error if left empty after pressing save
         
@@ -36,9 +62,9 @@ public class Main extends Application {
         
         //save button
             //When pressed, will close this page and lead back to homepage
-        Button save = new Button("Save");
-        //cancel button
-        Button cancel = new Button("Cancel");
+//        Button save = new Button("Save");
+//        //cancel button
+//        Button cancel = new Button("Cancel");
 	    
 	    
 	    
