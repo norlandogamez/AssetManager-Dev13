db1ad8fea9419b83e7ac2ed107bae862d6f3b0e7
Dylan Huang <dylanhuang119@gmail.com>
2024-03-24 20:19:16 -0700

    Added text fields, buttons, labels for Add Category page.

--- AssetManager/src/application/Main.java
+++ AssetManager/src/application/Main.java
@@ -43,11 +43,22 @@ public class Main extends Application {
         
         
         FlowPane root2 = new FlowPane();
+        // Category Name text field
         TextField category_name = new TextField();
+        
+        // Location Name text field
         TextField location_name= new TextField();
+        
+        // Location Description text field
         TextField location_desc = new TextField();
+        
+        // Save button
         Button add_category_save = new Button("Save");
+        
+        // Cancel button 
         Button add_category_cancel = new Button("Cancel");
+        
+        // Title and labels
         Label add_category_title = new Label("Add New Category");
         Label catname_label = new Label("Category Name:");
         Label locname_label = new Label("Location Name:");
@@ -68,8 +79,9 @@ public class Main extends Application {
         add_category_cancel.setOnAction(e -> {
             ((Stage) (add_category_cancel.getScene().getWindow())).close(); });
         
+        // Checks to see if category name and location name are filled, then closes page if so
         add_category_save.setOnAction(e -> {
-        	if (add_category_title.getText() == null || add_category_title.getText().trim().isEmpty() || location_name.getText() == null || location_name.getText().trim().isEmpty()) {
+        	if (category_name.getText() == null || category_name.getText().trim().isEmpty() || location_name.getText() == null || location_name.getText().trim().isEmpty()) {
         	     return;
         	}
             ((Stage) (add_category_save.getScene().getWindow())).close(); });
