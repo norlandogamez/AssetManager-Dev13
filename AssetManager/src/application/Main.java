321d0a2e0b036800092c2e2a448ab3fa73d4f0b0
norlandogamez <norlandogamez1@gmail.com>
2024-03-24 23:35:51 -0700

    Finished comments, layout, and colors.

--- AssetManager/src/application/Main.java
+++ AssetManager/src/application/Main.java
@@ -12,23 +12,19 @@ import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 	//Home page
+	    
+	    //Creates GridPane
 	    GridPane root = new GridPane();
 	    
         // View Assets button
@@ -43,25 +39,29 @@ public class Main extends Application {
         // Add New Category button
         // This button will lead to a new stage/page
         Button add_new_category = new Button("Add New Category");
-        
-        ButtonBar btnBar = new ButtonBar();
-        
-        btnBar.getButtons().addAll(view_assets, new_asset, view_categories, add_new_category);
-        
+                
         // Add icon image
-//        root.getChildren().addAll(view_assets, new_asset, view_categories, add_new_category);
-//        ImageView img1 = new ImageView(new Image(getClass().getResource()));
+        Image logo = new Image(getClass().getResource("trackit-logo.png").toString());
+        ImageView img1 = new ImageView(logo);
+        //Set image size
+        img1.setFitHeight(100);
+        img1.setFitWidth(100);
         
+       
         //Grid positioning
-        root.add(view_assets, 0, 0, 1, 1);
-        root.add(new_asset, 1, 0, 1, 1);
-        root.add(view_categories, 0, 1, 1, 1);
-        root.add(add_new_category, 1, 1, 1, 1);
-        root.setHgap(100);
+        root.add(view_assets, 0, 1, 1, 1);
+        root.add(new_asset, 2, 1, 1, 1);
+        root.add(view_categories, 0, 2, 1, 1);
+        root.add(add_new_category, 2, 2, 1, 1);
+        root.add(img1, 1, 0, 1, 1);
+        root.setHgap(0);
         root.setVgap(40);
         
+        //Sets alignment of nodes to the center and sets background color
+        root.setStyle("-fx-background-color: #9BDDF2");
         root.setAlignment(Pos.CENTER);
         
+        //Set home page to the scene
         Scene scene = new Scene(root, 500, 500);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Home Page");
@@ -84,24 +84,33 @@ public class Main extends Application {
         // Cancel button 
         Button add_category_cancel = new Button("Cancel");
         
-        // Title and labels
-        Label add_category_title = new Label(" Add New Category");
-        
+        //Labels for textfields
         Label catname_label = new Label("Category Name:");
         Label locname_label = new Label("Location Name:");
         Label locdesc_label = new Label("Location Description:");
         
-        root2.getChildren().addAll(add_category_title, catname_label, category_name, locname_label, location_name, locdesc_label, location_desc, add_category_save, add_category_cancel);
+        //Add image for second page
+        Image logo2 = new Image(getClass().getResource("trackit-logo.png").toString());
+        ImageView img2 = new ImageView(logo2);
+        img2.setFitHeight(100);
+        img2.setFitWidth(100);
+        
+        
+        //Grab all children for add new category page
+        root2.getChildren().addAll(img2, catname_label, category_name, locname_label, location_name, locdesc_label, location_desc, add_category_save, add_category_cancel);
         Scene secondScene = new Scene(root2, 500, 500);
         Stage secondStage = new Stage();
         
+        
+        
+        //Setting scene for Add New Category page
         secondStage.setScene(secondScene);
         secondStage.setTitle("Add New Category");
         
+        
+        //Aligns the VBox to the center and sets the background color
         root2.setAlignment(Pos.CENTER);
-        
-        
-
+        root2.setStyle("-fx-background-color: #9BDDF2");
         
         
         
