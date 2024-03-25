739f7852157b7389db339ee8f4e595588ba1ff64
Dylan Huang <dylanhuang119@gmail.com>
2024-03-24 21:29:51 -0700

    Updated comments in Main.java.

--- AssetManager/src/application/Main.java
+++ AssetManager/src/application/Main.java
@@ -20,28 +20,30 @@ public class Main extends Application {
 	//Home page
 	    FlowPane root = new FlowPane();
 	    
-        //view assets button
+        // View Assets button
 	    Button view_assets = new Button("View Assets");
 	    
-        //add new asset button
+        // Add New Asset button
 	    Button new_asset = new Button("Add New Asset");
-        //view categories button
-        Button view_categories = new Button("View Categories");
-        //add new category button
-            //This button will lead to a new stage/page
-        Button add_new_category = new Button("Add New Category");
-        //Add icon image
 	    
+        // View Categories button
+        Button view_categories = new Button("View Categories");
+        
+        // Add New Category button
+        // This button will lead to a new stage/page
+        Button add_new_category = new Button("Add New Category");
+        
+        // Add icon image
         root.getChildren().addAll(view_assets, new_asset, view_categories, add_new_category);
         
         Scene scene = new Scene(root, 500, 500);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Home Page");
         primaryStage.show();
-	//Add new category page
-        
-        
+	
+        // Add new category page
         FlowPane root2 = new FlowPane();
+        
         // Category Name text field
         TextField category_name = new TextField();
         
@@ -70,33 +72,20 @@ public class Main extends Application {
         secondStage.setScene(secondScene);
         secondStage.setTitle("Add New Category");
         
-        // Opens Add Category page
+        // Add New Category button opens Add Category page
         add_new_category.setOnAction(e -> {
             secondStage.show(); });
             
-        // Closes Add Category page
+        // Cancel button closes Add Category page
         add_category_cancel.setOnAction(e -> {
             ((Stage) (add_category_cancel.getScene().getWindow())).close(); });
         
-        // Checks to see if category name and location name are filled, then closes page if so
+        // Save button checks to see if category name and location name are filled, then closes page if so
         add_category_save.setOnAction(e -> {
         	if (category_name.getText() == null || category_name.getText().trim().isEmpty() || location_name.getText() == null || location_name.getText().trim().isEmpty()) {
         	     return;
         	}
             ((Stage) (add_category_save.getScene().getWindow())).close(); });
-        
-        //Category name textfield
-            //error if left empty after pressing save
-        
-    
-        //Category Description textfield
-            //error if left empty after pressing save
-        
-        //save button
-            //When pressed, will close this page and lead back to homepage
-//        Button save = new Button("Save");
-//        //cancel button
-//        Button cancel = new Button("Cancel");
 	    
 	}
 	
