d4f06b5edb65a772e53beea98eef2872476466df
norlandogamez <norlandogamez1@gmail.com>
2024-03-29 18:52:35 -0700

    UI implementation of the "Define New Location Page" complete.

--- AssetManager/src/application/Main.java
+++ AssetManager/src/application/Main.java
@@ -8,10 +8,13 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -34,7 +37,7 @@ public class Main extends Application {
 	    Button new_asset = new Button("Add New Asset");
 	    
         // View Categories button
-        Button view_categories = new Button("View Categories");
+        Button define_new_location = new Button("Define New Location");
         
         // Add New Category button
         // This button will lead to a new stage/page
@@ -51,7 +54,7 @@ public class Main extends Application {
         //Grid positioning
         root.add(view_assets, 0, 1, 1, 1);
         root.add(new_asset, 2, 1, 1, 1);
-        root.add(view_categories, 0, 2, 1, 1);
+        root.add(define_new_location, 0, 2, 1, 1);
         root.add(add_new_category, 2, 2, 1, 1);
         root.add(img1, 1, 0, 1, 1);
         root.setHgap(0);
@@ -72,12 +75,6 @@ public class Main extends Application {
         // Category Name text field
         TextField category_name = new TextField();
     
-        // Location Name text field
-        TextField location_name= new TextField();
-        
-        // Location Description text field
-        TextField location_desc = new TextField();
-        
         // Save button
         Button add_category_save = new Button("Save");
         
@@ -86,8 +83,6 @@ public class Main extends Application {
         
         //Labels for textfields
         Label catname_label = new Label("Category Name:");
-        Label locname_label = new Label("Location Name:");
-        Label locdesc_label = new Label("Location Description:");
         
         //Add image for second page
         Image logo2 = new Image(getClass().getResource("trackit-logo.png").toString());
@@ -97,7 +92,7 @@ public class Main extends Application {
         
         
         //Grab all children for add new category page
-        root2.getChildren().addAll(img2, catname_label, category_name, locname_label, location_name, locdesc_label, location_desc, add_category_save, add_category_cancel);
+        root2.getChildren().addAll(img2, catname_label, category_name,add_category_save, add_category_cancel);
         Scene secondScene = new Scene(root2, 500, 500);
         Stage secondStage = new Stage();
         
@@ -114,6 +109,12 @@ public class Main extends Application {
         
         
         
+        define_new_location.setOnAction(e -> {
+            DefineNewLocation();
+            
+        });
+        
+        
         // Add New Category button opens Add Category page
         add_new_category.setOnAction(e -> {
             secondStage.show(); });
@@ -124,10 +125,54 @@ public class Main extends Application {
         
         // Save button checks to see if category name and location name are filled, then closes page if so
         add_category_save.setOnAction(e -> {
-        	if (category_name.getText() == null || category_name.getText().trim().isEmpty() || location_name.getText() == null || location_name.getText().trim().isEmpty()) {
+        	if (category_name.getText() == null || category_name.getText().trim().isEmpty()){
         	     return;
         	}
             ((Stage) (add_category_save.getScene().getWindow())).close(); });
+	    
+	}
+	
+	
+	public void DefineNewLocation() {
+	    VBox locRoot = new VBox();
+	    
+	    TextField locationName_field = new TextField(); 
+	    
+	    Label location_label = new Label("Location's name:");
+	    
+	    TextField locationDesc_field = new TextField();
+	    
+	    Label locationDesc_label = new Label("Location Description (optional)");
+	    
+	    Button save = new Button("Save");
+	    
+	    Button cancel = new Button("Cancel");
+	    
+	    locRoot.getChildren().addAll(location_label, locationName_field,locationDesc_label , locationDesc_field,save, cancel);
+	    
+	    Scene newScene = new Scene(locRoot, 500, 500);
+	    
+	    Stage newStage = new Stage();
+	    
+	    newStage.setScene(newScene);
+	    newStage.setTitle("New Location");
+	    
+	    locRoot.setAlignment(Pos.CENTER);
+	    locRoot.setStyle("-fx-background-color: #9BDDF2");
+	    
+	    newStage.show();
+	    
+	    cancel.setOnAction(e -> {
+	        ((Stage) (cancel.getScene().getWindow())).close(); 
+	    });
+	    
+	    save.setOnAction(e -> {
+            if (locationName_field.getText() == null || locationName_field.getText().trim().isEmpty()){
+                 return;
+            }
+            ((Stage) (save.getScene().getWindow())).close(); });
+        
+	    
 	    
 	}
 	
